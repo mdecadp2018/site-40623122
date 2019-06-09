4151d1975f7dd53fbec6556864cc1cbc500ef703
40623122 <40623122@gm.nfu.edu.tw>
2019-06-09 23:30:07 +0800

    code

--- vrep/AvA.py
+++ vrep/AvA.py
@@ -1,5 +1,5 @@
 import vrep
-import keyboard
+import keyboard    #導入鍵盤
 import time
 import sys, math     
 # child threaded script: 
@@ -10,34 +10,33 @@ vrep.simxFinish(-1)
 
 
 clientID = vrep.simxStart('127.0.0.1', 19997, True, True, 5000, 5)
-KickBallV = 360
-Move_Minus =-0.1        
-Move_Plus =0.1
-n=1
-R_KickBallVel = (math.pi/180)*KickBallV
+
+KickBallV = 360     #轉軸角度
+
+R_KickBallVel = (math.pi/180)*KickBallV   
 B_KickBallVel = -(math.pi/180)*KickBallV
+
 if clientID!= -1:
     print("Connected to remote server")
 else:
     print('Connection not successful')
-    sys.exit('Could not connect')
+    sys.exit('Could not connect')                                  #以上為連動程式
     
-errorCode,Sphere_handle=vrep.simxGetObjectHandle(clientID,'Sphere',vrep.simx_opmode_oneshot_wait)
 errorCode,P1_handle=vrep.simxGetObjectHandle(clientID,'P1',vrep.simx_opmode_oneshot_wait)
 errorCode,P2_handle=vrep.simxGetObjectHandle(clientID,'P2',vrep.simx_opmode_oneshot_wait)
 errorCode,R1_handle=vrep.simxGetObjectHandle(clientID,'R1',vrep.simx_opmode_oneshot_wait)
 errorCode,R2_handle=vrep.simxGetObjectHandle(clientID,'R2',vrep.simx_opmode_oneshot_wait)
-
-
+ 
 vrep.simxSetJointTargetVelocity(clientID,P1_handle,0,vrep.simx_opmode_oneshot_wait)
 vrep.simxSetJointTargetVelocity(clientID,P2_handle,0,vrep.simx_opmode_oneshot_wait)
 vrep.simxSetJointTargetVelocity(clientID,R1_handle,0,vrep.simx_opmode_oneshot_wait)
 vrep.simxSetJointTargetVelocity(clientID,R2_handle,0,vrep.simx_opmode_oneshot_wait)
 
-def speed(handle,speed):
+def speed(handle,speed):    #定義速度
     errorCode = vrep.simxSetJointTargetVelocity(clientID,handle,speed,vrep.simx_opmode_oneshot_wait)
 
 vrep.simxStartSimulation(clientID,vrep.simx_opmode_oneshot_wait)
+
 while True:
     try:
             if keyboard.is_pressed('s'): 
@@ -66,4 +65,3 @@ while True:
                 speed(P2_handle,0)
     except:
             break
-#vrep.simxSetJointTargetVelocity(clientID,R1_handle,0,vrep.simx_opmode_oneshot_wait)
